90c4be45b2e73e05ccd8672dbd55f3e82ae6cedb
plas12 <143487328+plas12@users.noreply.github.com>
2024-03-06 16:49:23 +0900

    Add files via upload

---
+++ 240306.py
@@ -0,0 +1,104 @@
+
+'''
+print("Hallo", end = ' ')기본이 \n
+print("World")
+
+
+print("Hallo", "World", sep='')#기본이  ' '
+
+
+a = input()
+print(a)
+
+
+print(input())
+
+
+input("학교 : ")
+input("학년 : ")
+
+
+print(input("What's your favorite frut?"))
+
+
+print("\"\'습관\'보다\n\t강한 것은 없다.\"")
+
+
+import turtle
+
+
+import turtle
+turtle.shape("turtle")
+turtle.circle(50)
+turtle.done()
+
+
+import turtle
+turtle.shape("turtle")
+turtle.write("Welcome to Ptyhon World !")
+turtle.done
+
+
+import turtle
+turtle.shape("turtle")
+turtle.circle(100)
+turtle.write("Hello World", font = ("", 30, ""))
+turtle.dome
+
+
+import turtle
+turtle.shape("turtle")
+turtle.circle(100)
+turtle.write("Welcome to Python World !", True, font = ("Arial", 18, "bold"))
+turtle.circle(18, 210)
+turtle.done()
+
+
+import turtle
+turtle.shape("turtle")
+turtle.circle(100,steps = 6)
+
+
+import turtle
+turtle.circle(100)
+turtle.circle(100, 720, steps = 5)
+#turtle.circle(100, 144, steps = 1)
+#turtle.circle(100, 144, steps = 1)
+#turtle.circle(100, 144, steps = 1)
+#turtle.circle(100, 144, steps = 1)
+#turtle.circle(100, 144, steps = 1)
+
+
+import turtle
+turtle.circle(100)
+turtle.circle(100, steps = 8)
+turtle.circle(100, steps = 4)
+
+
+print(input("이름 :" ))
+print(input("학년 :" ))
+print(input("반 :" ))
+
+
+import turtle
+turtle.circle(100, steps = 4)
+turtle.circle(50, steps = 4)
+turtle.circle(100, 180, steps = 1)
+
+
+import turtle
+turtle.circle(100, steps = 4)
+turtle.circle(50, steps = 4)
+turtle.circle(100, 180, steps = 1)
+turtle.circle(50, steps = 4)
+'''
+
+import turtle
+turtle.circle(50, steps = 8)
+turtle.circle(100, 180, steps = 1)
+turtle.circle(50, steps = 6)
+turtle.circle(150, 180, steps = 1)
+turtle.circle(50, steps = 10)
+turtle.circle(200, 180, steps = 1)
+turtle.circle(50, steps = 4)
+turtle.circle(250, 180, steps = 1)
